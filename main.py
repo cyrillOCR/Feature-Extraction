fd2f3d74bb43104066930f877c0bd8783396566a
3DDD <ioan.bodnar@info.uaic.ro>
2018-11-29 15:21:30 +0200

    Mean margins from left to right

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from modules.resize_image import resize
 from modules.dct import dct_function
 from modules.quantize_transformation_coeff import quantize_function
 from modules.zig_zag_traversal import zig_zag
+from modules.mean_margins_left_right import mean_margins_left_right
 
 if __name__ == "__main__":
     try:
@@ -15,11 +16,12 @@ if __name__ == "__main__":
         img = image_vector(sys.argv[1])
         dictionary = {}
         dictionary['quadrans_sum'] = quadrans_sum(img)
+        dictionary['mean-margins-left-right'] = mean_margins_left_right(img)
         resize_result = resize(img)
         dct_result = dct_function(resize_result)
         quantize_result = quantize_function(dct_result)
         vectorize_coeff_result = zig_zag(quantize_result)
-        print (vectorize_coeff_result)
+        print (dictionary)
 
     except:
         pass
