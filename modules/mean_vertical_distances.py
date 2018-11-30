a487bac0235f96cb2fc268e156807db35e2c2549
Emilia <sfirnaciuc_emilia@yahoo.com>
2018-11-30 20:10:41 +0200

    mean of vertical distances between "on" pixels - optimized

--- modules/mean_vertical_distances.py
+++ modules/mean_vertical_distances.py
@@ -1,3 +1,4 @@
+
 def distance(i,j,image):
     dis=0
     find=0
@@ -8,25 +9,25 @@ def distance(i,j,image):
         else:
             dis+=1
     if find==1 :
-        return dis
+        return dis,k
     else:
-        return 0
+        return 0,k
 
 
 def mean_vertical_distance(image):
     sum_of_distances=0
     total_distances=0
-    for i in range(0, len(image)):
-        for j in range(0, len(image[i])):
+    j=0
+    while j<len(image[0]):
+        i=0
+        while i<len(image):
             if image[i][j]<=200 and i+1<len(image)-1:
                 if image[i+1][j]>200:
-                   distance_pixel=distance(i,j,image)
+                   distance_pixel,k=distance(i,j,image)
                    sum_of_distances+=distance_pixel
+                   i=k
                    if distance_pixel!=0:
                        total_distances+=1
+            i+=1
+        j+=1
     return sum_of_distances/total_distances
-
-#image=[[255,255,255,255,255,255,255],[255,255,0,0,0,0,255],[255,255,0,0,0,0,255],[255,0,0,255,255,255,255],[255,0,0,255,255,255,255],[255,0,0,255,255,255,255],[255,255,0,0,0,0,255],[255,255,0,0,0,0,255]]
-#print(mean_vertical_distance(image))
-
-
