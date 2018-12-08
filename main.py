c1230c61a8990d01742c8afd4d84f658185065a7
3DDD <ioan.bodnar@info.uaic.ro>
2018-12-09 00:49:43 +0200

    means and dct sub modules created

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 import sys
 
-from cv2.cv2 import dct
 from modules.iterate_image import image_vector
-from modules.quadrans import quadrans_sum
-from modules.resize_image import resize
-from modules.dct import dct_function
-from modules.quantize_transformation_coeff import quantize_function
-from modules.zig_zag_traversal import zig_zag
-from modules.mean_margins_left_right import mean_margins_left_right
-from modules.mean_vertical_horizontal_on import mean_vertical_horizontal_on,mean_square_vertical_horizontal_on
-from modules.mean_square_horizontal_vertical import mean_square_horizontal_vertical
-#from modules.mean_margins_bottom_top import mean_margins_bottom_top
+from modules.means.mean_margins_bottom_top import mean_margins_bottom_top
+from modules.means.mean_margins_left_right import mean_margins_left_right
+from modules.means.mean_square_horizontal_vertical import mean_square_horizontal_vertical
+from modules.means.mean_vertical_horizontal_on import mean_square_vertical_horizontal_on
+from modules.dct.resize_image import resize
+from modules.dct.dct import dct_function
+from modules.dct.quantize_transformation_coeff import quantize_function
+from modules.dct.zig_zag_traversal import zig_zag
 if __name__ == "__main__":
     try:
-        print ("Main function")
+        vectored_image = image_vector(sys.argv[1])
 
-        img = image_vector(sys.argv[1])
-        dictionary = {}
-        dictionary['quadrans_sum'] = quadrans_sum(img)
-        dictionary['mean-margins-left-right'] = mean_margins_left_right(img)
-        dictionary['mean_square_vertical_horizontal_on'] = mean_square_vertical_horizontal_on(img)
-        dictionary['mean_vertical_horizontal_on'] = mean_vertical_horizontal_on(img)
-        dictionary['mean_square_horizontal_vertical'] = mean_square_horizontal_vertical(img)
-       # dictionary['mean_margins_bottom_top'] = mean.mean_margins_bottom_top
-       # resize_result = resize(img)
-        #dct_result = dct_function(resize_result)
-        #quantize_result = quantize_function(dct_result)
-        #vectorize_coeff_result = zig_zag(quantize_result)
-        print (dictionary)
+        print(mean_margins_bottom_top(vectored_image))
+        print(mean_margins_left_right(vectored_image))
+        print(mean_square_horizontal_vertical(vectored_image))
+        print(mean_square_vertical_horizontal_on(vectored_image))
 
-    except:
-        pass
+        resize = resize(vectored_image)
+        dct_result = dct_function(resize)
+        qr = quantize_function(dct_result)
+        vect_q = zig_zag(qr)
+
+        print(vect_q)
+
+        
+
+        print("Finished")
+    except Exception as e:
+        print (str(e))
 
 
