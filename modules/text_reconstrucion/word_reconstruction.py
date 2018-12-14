4c9ba8450c86a21fe6cc1ea67375b5beeaf73613
andrada97 <andradaa.didaa@gmail.com>
2018-12-14 20:53:41 +0200

    reconstructie linie-cuvant

--- modules/text_reconstrucion/word_reconstruction.py
+++ modules/text_reconstrucion/word_reconstruction.py
@@ -1,9 +1,5 @@
-#
 def textReconstruction(letterList,file,prag_cuvant,prag_rand):
 
-    finalText=[]
-   # finalText.append("mama")
-    #print(finalText[0][0])
     fd = open(file, "r")
     index = fd.readlines()
     list_of_coords = []
@@ -15,37 +11,39 @@ def textReconstruction(letterList,file,prag_cuvant,prag_rand):
         list_of_coords[i]=tuple(list_of_coords[i])
         print(list_of_coords[i])
 
-    first=list_of_coords[0]
-    #for l in list_of_coords:
-    currentWord=[]
-    line=0
-    word=0
-    letter=0
-    print(letterList[0])
-    finalText.append(letterList[0])
-    letter+=1
-    for l in range(1,len(list_of_coords)):
 
+    finalText=[]
+    lineText=[]
+    wordText=[]
+    wordText.append(letterList[0])
+
+    for l in range(1,len(list_of_coords)):
         x1, y1, x2, y2 = list_of_coords[l][3], list_of_coords[l][0], list_of_coords[l][1], list_of_coords[l][2]
+
         if x1-list_of_coords[l-1][1]>prag_cuvant:
             print("cuvant nou")
-            word+=1
-            letter=0
+            lineText.append(wordText)
+            wordText.clear()
+            wordText.append(letterList[l])
 
 
-        if y1-list_of_coords[l-1][2]>prag_rand:
+        elif y1-list_of_coords[l-1][2]>prag_rand:
             print("rand nou")
-            line+=1
-            word=0
-            letter=0
+            finalText.append(lineText)
+            lineText.clear()
+            wordText.clear()
+            wordText.append(letterList[l])
 
-        else:
-            letter+=1
-        finalText[line][word][letter]=letterList[l]
 
-    print(finalText)
+        else :
+            wordText.append(letterList[l])
+
+    for i in range(0,len(finalText)):
+        print(finalText[i])
+
+
 def main():
     prag_cuvant=1
     prag_rand=1
-    textReconstruction(['a','b','c','d','e'], "C:\\Users\\vasil\\PycharmProjects\\taskAI\\input.txt",prag_cuvant,prag_rand)
+    textReconstruction(['a','b','c','d','e'], "C:\\Users\\Andrada\\Desktop\\AI_Modul\\input.txt",prag_cuvant,prag_rand)
 main()
