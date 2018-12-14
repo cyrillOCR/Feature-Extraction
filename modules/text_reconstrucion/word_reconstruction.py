a45c8073d4b4a8830504d079f655b18f438d85a8
MirunaElena <mirunavasilescu97@gmail.com>
2018-12-15 00:24:51 +0200

    testare cu Input manual de 98 caractere (text Maitreyi)

--- modules/text_reconstrucion/word_reconstruction.py
+++ modules/text_reconstrucion/word_reconstruction.py
@@ -21,7 +21,7 @@ def textReconstruction(letterList,file,prag_cuvant,prag_rand):
         x1, y1, x2, y2 = list_of_coords[l][3], list_of_coords[l][0], list_of_coords[l][1], list_of_coords[l][2]
 
         if x1-list_of_coords[l-1][1]>prag_cuvant:
-            print("cuvant nou")
+            #print("cuvant nou")
             aux=[]
             aux=wordText.copy()
             lineText.append(aux)
@@ -30,7 +30,7 @@ def textReconstruction(letterList,file,prag_cuvant,prag_rand):
 
 
         elif y1-list_of_coords[l-1][2]>prag_rand:
-            print("rand nou")
+            #print("rand nou")
             aux2=[]
             aux2 = wordText.copy()
             lineText.append(aux2)
@@ -60,6 +60,13 @@ def textReconstruction(letterList,file,prag_cuvant,prag_rand):
 def main():
     prag_cuvant=1
     prag_rand=1
-    textReconstruction(['a', 'b', 'c', 'd', 'e'], "C:\\Users\\sfirn\\Desktop\\AI\\input.txt", prag_cuvant, prag_rand)
-
+    #textReconstruction(['a', 'b', 'c', 'd', 'e'], "C:\\Users\\sfirn\\Desktop\\AI\\input.txt", prag_cuvant, prag_rand)
+    textReconstruction(['E', 'm', 'i', 'e', 'z', 'u', 'l', 'n', 'o', 'p', 'ţ', 'i', 'i', ',',  # 14
+                        'p', 'o', 'a', 't', 'e', 'f', 'i', 'ş', 'i', 'd', 'o', '-',  # 12
+                        'u', 'ă', 'n', 'o', 'a', 'p', 't', 'e', 'a', '.',  # 10
+                        'S', 'u', 'n', 't', 's', 'i', 'n', 'g', 'u', 'r', 'ă', ',', 'p', 'e', 'v', 'e', '-',  # 17
+                        'r', 'a', 'n', 'd', 'ă', '.', 'D', 'e', 'a', 'i', 'c', 'i', 'n', 'u',  # 14
+                        'p', 'o', 't', 'c', 'u', 'p', 'r', 'i', 'n', 'd', 'e', 't', 'o', 't', 'c', 'e', '-',  # 17
+                        'r', 'u', 'l', 'c', 'u', 'p', 'r', 'i', 'v', 'i', 'r', 'e', 'a', '.']  # 14
+                       , "C:\\Users\\vasil\\PycharmProjects\\taskAI\\input.txt", prag_cuvant, prag_rand)
 main()
