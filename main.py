9460a1987b7a804f3a468e967761b17054ea4689
Ciprian <cyprian.bodnar@gmail.com>
2019-01-04 18:09:18 +0200

    making a server to run with flask

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import sys
 import time
 
+from flask import Flask
+
 from modules.dct.dct_for_each_letter import dct_means_for_each_letter_function
 from modules.utils.Resize import crop
 from modules.utils.iterate_image import image_vector
 from modules.utils.json import make_json
 
-if __name__ == "__main__":
+app = Flask(__name__)
+
+@app.route('/',methods = ['POST'])
+def requests():
     try:
         t = time.time()
         vectored_image = image_vector(sys.argv[1])
@@ -14,28 +19,14 @@ if __name__ == "__main__":
         file = sys.argv[2]
         f = open("test.txt", "w")
         result = dct_means_for_each_letter_function(crop(page, file))
-
-        for letter in result:
-            test_len = 0
-            # for i in letter:
-            # for j in i:
-            #     test_len += len(j)
-            # print(letter)
-            # print('\n')
-            # print(test_len)
-            # print('\n\n')
-            f.write(str(letter))
-            f.write('\n\n')
-            # f.write(str(test_len))
-
         make_json(result)
 
-        # for letter in dct_means_for_each_letter_function(crop(page, file)):
-        #     print(letter)
-        #     print('\n\n')
-        #
         print(time.time() - t)
         print("Finished")
 
     except Exception as e:
         print(str(e))
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0',port = 80,debug=True)
