152a50f7d6d7d395fca96ec9f85fd2d7416b2f62
Ciprian <cyprian.bodnar@gmail.com>
2019-01-04 21:00:27 +0200

    POST request with a data set of coords in json format. the result is send back in json formar

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 import sys
 import time
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 from modules.dct.dct_for_each_letter import dct_means_for_each_letter_function
 from modules.utils.Resize import crop
 from modules.utils.iterate_image import image_vector
 from modules.utils.json import make_json
+from modules.utils.coordSerialization import coordSerialization
 
 app = Flask(__name__)
 
 @app.route('/',methods = ['POST'])
 def requests():
-    try:
-        t = time.time()
-        vectored_image = image_vector(sys.argv[1])
-        page = sys.argv[1]
-        file = sys.argv[2]
-        f = open("test.txt", "w")
-        result = dct_means_for_each_letter_function(crop(page, file))
-        make_json(result)
-
-        print(time.time() - t)
-        print("Finished")
-
-    except Exception as e:
-        print(str(e))
+   
+    coordsList = []
+    # functie care transforma din base64 in imagine normala
+    page = 'images/i5bw.jpg'
+    coords_json = request.json
+    
+    # de schimbat aici cu scriptul de serializarea a coordonatelor care trebuie modificat
+    for i in coords_json['coords']:
+        coordsList.append(tuple(i))
+    # am folosit scriptul 'Resize' trebuie copiat in alt script
+    result = dct_means_for_each_letter_function(crop(page, coordsList))
+    return jsonify(make_json(result)), 200
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0',port = 80,debug=True)
+    app.run(port = 5005,debug=True)
--- modules/utils/Resize.py
+++ modules/utils/Resize.py
@@ -1,66 +1,27 @@
 import copy
 
 import cv2
-import numpy as np
-
-ox_max = 65
-oy_max = 65
+from numpy import array
 
 
-def crop(img, file):
+def crop(img, list_of_coords):
     image = cv2.imread(img, 0)
-    a = np.uint8(255)
-    fd = open(file, "r")
-    index = fd.readlines()
-    list_of_coords = []
-    # print(list_of_coords)
-
-    for i in range(len(index)):
-        # print(index[i])
-        list_of_coords.append(index[i].lstrip('(').rstrip(')\n').split(','))
-
-    # print(list_of_coords)
-
-    for i in range(len(list_of_coords)):
-        for j in range(4):
-            list_of_coords[i][j] = int(list_of_coords[i][j])
-        list_of_coords[i] = tuple(list_of_coords[i])
+    number_of_letters = 0
     aux = []
     cropped = []
-
-    number_of_letters = 0
-    for (y1, x1, y2, x2) in list_of_coords:
-        copy_x = x1
-        aux_ox = x1 + ox_max - 1
-        while (copy_x <= aux_ox):
+    for i in range(len(list_of_coords)):
+        for j in range(list_of_coords[i][1], list_of_coords[i][3] + 1):
             aux.append([])
-            copy_x += 1
         cropped.append(copy.deepcopy(aux))
         del aux[:]
-        aux_ox = x1 + ox_max - 1
-        aux_oy = y1 + oy_max - 1
-        copy_x = x1
-        copy_y = y1
-
+    index = 0
+    for (y1, x1, y2, x2) in list_of_coords:
         row_of_letter = 0
-        while (copy_x <= aux_ox):
-            copy_y = y1
-
-            while (copy_y <= aux_oy):
-                if copy_x <= x2 and copy_y <= y2:
-                    cropped[number_of_letters][row_of_letter].append(image[copy_x][copy_y])
-                else:
-                    cropped[number_of_letters][row_of_letter].append(a)
-
-                copy_y += 1
-
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                cropped[number_of_letters][row_of_letter].append(image[i][j])
             row_of_letter += 1
-            copy_x += 1
         number_of_letters += 1
-
-    # imagine = array(cropped[6])
-    # cv2.imshow('image', imagine)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
+        index = index +1
     return cropped
+
