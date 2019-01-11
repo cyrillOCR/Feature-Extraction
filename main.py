f14b8f54b3c9f575ce1538df8a6fce7a589dd7fc
Ciprian <cyprian.bodnar@gmail.com>
2019-01-11 23:44:51 +0200

    letters returnen in jsonify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from modules.dct.dct_for_each_letter import dct_means_for_each_letter_function
 from modules.utils.Resize_optimizat_1 import crop
 from modules.utils.json import make_json,coordSerialization
@@ -8,17 +9,23 @@ from modules.predicted.predict import predict
 import json
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/feature',methods=['POST','OPTIONS'])
 def requests():
     received_json = request.json
     coords_from_json=received_json["coords"]
     base64_from_json=received_json["base64"]
     coordslist=coordSerialization(coords_from_json)
+
+
     result = dct_means_for_each_letter_function(crop(decode(base64_from_json), coordslist))
     predictedResult = predict(make_json(result))
-    return jsonify(predictedResult), 200 
+    return jsonify(predictedResult), 200 , {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        }
+   
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(port=5005)
