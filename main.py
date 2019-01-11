ff4a18594da16769669f5e0a64642304263fb02a
laura <laura.bejan@info.uaic.ro>
2019-01-11 22:46:53 +0200

    Fixed main

--- main.py
+++ main.py
@@ -11,20 +11,16 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/',methods=['POST'])
+@app.route('/feature',methods=['POST','OPTIONS'])
 def requests():
     received_json = request.json
     coords_from_json=received_json["coords"]
     base64_from_json=received_json["base64"]
     coordslist=coordSerialization(coords_from_json)
     result = dct_means_for_each_letter_function(crop(decode(base64_from_json), coordslist))
-    print(make_json(result))
     predictedResult = predict(make_json(result))
-    #puts the predicted result in a json file
-    with open('modules/predicted/predictedResult.json','w') as outfile:
-        json.dump(predictedResult,outfile)
-    return jsonify(make_json(result)), 200 
+    return jsonify(predictedResult), 200 
 
 
 if __name__ == "__main__":
-    app.run(port=5005,debug=True)
+    app.run(host='0.0.0.0')
