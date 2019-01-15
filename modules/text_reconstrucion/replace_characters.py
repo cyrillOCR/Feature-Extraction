e61d6dfc24dd96717f976d488a6ae8ade7ba01d4
Ciprian <cyprian.bodnar@gmail.com>
2019-01-15 20:59:17 +0200

    First attempt for text reconstruction

--- modules/text_reconstrucion/replace_characters.py
+++ modules/text_reconstrucion/replace_characters.py
@@ -11,7 +11,5 @@ def replace_character(finalText):
         replaced_text = replaced_text.replace("\u0219t1\u0219t2","\u0219t")
         replaced_text = replaced_text.replace("\u021B1\u021B2","\u021B")
 
-    print(replaced_text)
+    return replaced_text
 
-text="dacădumn1n2eataști .iliatinește ... Eudomnl n2 !\u02191\u02192 ... \u0219t1\u0219t2...\u0103"
-replace_character(text)
