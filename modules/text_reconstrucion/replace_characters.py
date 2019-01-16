07310ce1ae20f57ac8ea76c208c1ef39bc0887e6
Ciprian <cyprian.bodnar@gmail.com>
2019-01-16 02:45:22 +0200

    last commit for tonight

--- modules/text_reconstrucion/replace_characters.py
+++ modules/text_reconstrucion/replace_characters.py
@@ -18,7 +18,10 @@ def replace_character(list_of_words,dictionary_of_patterns):
     for word in list_of_words:
         word_copy = [word[0],word[1]]
         for key in dictionary_of_patterns:
-            word_copy[1] = word_copy[1].replace(key,dictionary_of_patterns[key]) 
+            word_copy[1] = word_copy[1].replace(key,dictionary_of_patterns[key])
+            word_copy[1] = word_copy[1].replace("\u02191\u02192","\u0219")
+            word_copy[1] = word_copy[1].replace("\u0219t1\u0219t2","\u0219t")
+            word_copy[1] = word_copy[1].replace("\u021B1\u021B2","\u021B")
         formatted_words.append(word_copy)
 
     return formatted_words
