fd5f129d92a83734a830e5ae8bbf19ee099db1d1
Carja Mihail <carja_mihail@yahoo.com>
2019-01-17 21:45:50 +0200

    Documentation for resize

--- modules/utils/Resize_optimizat_1.py
+++ modules/utils/Resize_optimizat_1.py
@@ -5,7 +5,7 @@ import numpy as np
 
 ox_max = 32
 oy_max = 32
-
+#set the size of the resized letters
 
 def crop(img, list_of_coords):
     image = cv2.imdecode(img, 0)
@@ -24,6 +24,7 @@ def crop(img, list_of_coords):
             copy_x += 1
         cropped.append(copy.deepcopy(aux))
         del aux[:]
+        #create the list where we will store the images for each letter
         aux_ox = x1 + ox_max - 1
         aux_oy = y1 + oy_max - 1
         copy_x = x1
@@ -36,8 +37,10 @@ def crop(img, list_of_coords):
             while (copy_y <= aux_oy):
                 if copy_x <= x2 and copy_y <= y2:
                     cropped[number_of_letters][row_of_letter].append(image[copy_x][copy_y])
+                    #we copy from image if the cursor is less then the actual size
                 else:
                     cropped[number_of_letters][row_of_letter].append(a)
+                    #we fill with white if the cursor is greater then the actual size
 
                 copy_y += 1
 
